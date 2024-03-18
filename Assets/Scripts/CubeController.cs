3b74af894477fcbfd6f2d2eed72cae5e45d43316
mjpurin <mjpurin@gmail.com>
2024-03-18 13:17:04 +0900

    p211

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -8,7 +8,9 @@ public class CubeController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(CubeMove());
         StartCoroutine(CubeRotate());
+        Debug.Log("hoge");
 
     }
         
@@ -17,11 +19,19 @@ public class CubeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(vec);
+        //transform.Rotate(vec);
+        if(Input.GetButtonDown("Fire1")){
+            
+        }
     }
     /*
     戻り地の型はIEnumerator
     最低１回はyield return の記述が必要
+    複数 yeild returnの記述可能
+    yeild return後ろでnew WaitForSeconds(秒数)
+    するとその時間処理を止めることができる
+    通信をする際にも利用される
+    StartCoroutineで起動
     */
     IEnumerator CubeRotate(){
         yield return new WaitForSeconds(1f);
@@ -32,5 +42,11 @@ public class CubeController : MonoBehaviour
         yield return new WaitForSeconds(2f);
         vec.y = 0f;
         vec.z = 1.0f;
+    }
+    IEnumerator CubeMove(){
+        while(true){
+            transform.Rotate(vec);
+            yield return null;
+        }
     }
 }
